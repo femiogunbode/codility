36b34a49eb9c53abf00cdb8300024eba7b1b57f3
femi <femi@femis-MacBook-Pro.local>
2021-10-23 17:44:32 +0100

    100% babyyy - BinaryGap.py

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,27 +1,45 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-def solution(N):
-    # write your code in Python 3.6
-    gapCounter = 0
-    tempCounter = 0
-    repeat = False
-    binary = list(format(N, "b"))
+'''
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 
-    for index, digit in enumerate(binary):
-        if (digit == '1') and (index == 0):
-            pass
-        elif digit == '0' and not repeat:
-            if index == (len(binary)-1):
-                gapCounter = 0
-            else:
-                gapCounter += 1
-        elif (digit == '1') and (index != len(binary)):
-            repeat = True
-            if tempCounter > gapCounter:
-                gapCounter = tempCounter
-        elif (digit == '0') and repeat:
-            tempCounter += 1
-        else:
-            pass
-    return gapCounter
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. 
+The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. 
+The number 20 has binary representation 10100 and contains one binary gap of length 1. 
+The number 15 has binary representation 1111 and has no binary gaps. 
+The number 32 has binary representation 100000 and has no binary gaps.
+
+Write a function:
+
+def solution(N)
+
+that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..2,147,483,647].
+'''
+
+def solutionV1(N):
+    # write your code in Python 3.6
+    gap_list = []
+    gap = 0
+    start = False
+
+    while N > 0:
+        if (N // 2 ) != (N / 2): #if remainder == 1
+            N = N // 2
+            start = True
+            if start: 
+                gap_list.append(gap)
+            gap = 0
+        elif start and ((N // 2 ) == (N / 2)):
+            N = N // 2
+            gap += 1
+        
+        else : N = N // 2
+
+    return max(gap_list)
